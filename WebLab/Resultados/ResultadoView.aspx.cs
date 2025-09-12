22c19b6f781469056099b6d7abbed34a2fa6b626
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2025-09-12 09:13:57 -0300

    fix: que muestre siempre el Resultado Car en cualquier estado de la derivacion

--- WebLab/Resultados/ResultadoView.aspx.cs
+++ WebLab/Resultados/ResultadoView.aspx.cs
@@ -763,17 +763,17 @@ namespace WebLab.Resultados
                         //    }
                         //    else
                         //    {
-                        if (oDeriva.Estado == 0) /// pendiente                            
-                        {
-                            estadoDerivacion = "Pendiente de Derivacion";
-                            lblDerivacion.ForeColor = Color.Red;
-                        }
-                        if (oDeriva.Estado == 1) /// enviado
-                            estadoDerivacion = oDetalle.ResultadoCar; //  "Derivado: " + oItem.GetEfectorDerivacion(oCon.IdEfector);
-                        if (oDeriva.Estado == 2) /// no enviado
-                            estadoDerivacion = oDetalle.ResultadoCar; //" No Derivado. " + oDeriva.Observacion;
-                        lblDerivacion.Font.Bold = true;
-
+                        /* if (oDeriva.Estado == 0) /// pendiente                            
+                         {
+                             estadoDerivacion = "Pendiente de Derivacion";
+                             lblDerivacion.ForeColor = Color.Red;
+                         }
+                         if (oDeriva.Estado == 1) /// enviado
+                             estadoDerivacion = oDetalle.ResultadoCar; //  "Derivado: " + oItem.GetEfectorDerivacion(oCon.IdEfector);
+                         if (oDeriva.Estado == 2) /// no enviado
+                             estadoDerivacion = oDetalle.ResultadoCar; //" No Derivado. " + oDeriva.Observacion;
+                         lblDerivacion.Font.Bold = true;*/
+                        estadoDerivacion = oDetalle.ResultadoCar;
                         if (oDeriva.Resultado != "")
                             estadoDerivacion += " - Resultado Informado: " + oDeriva.Resultado;
 
