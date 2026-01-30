f16f791c524ece641ab034a6eb4621992d06565e
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2026-01-30 09:50:03 -0300

    Se agrega carga de protocolo para Labo Central

--- WebLab/TestCypress/cypress/e2e/Protocol.cy.js
+++ WebLab/TestCypress/cypress/e2e/Protocol.cy.js
@@ -48,13 +48,21 @@
         cy.encabezadoProtocolo();
         cy.cargarDiagnostico();
 
+        //"LABORATORIO CENTRAL"
+        cy.fixture("datos").then(x => {
+            if (x.efector == "LABORATORIO CENTRAL") {
+                //Caracter SISA:
+                cy.get("select[id$='ddlCaracter']").select(x.caracterSisa);
+            }
+        });
+
         cy.get('body').click(0, 0);
         cy.wait(1000); 
-        // 2. Interceptar el guardado para obligar a Cypress a esperar la respuesta
+        // Interceptar el guardado para obligar a Cypress a esperar la respuesta
         cy.intercept('POST', '**/ProtocoloEdit2.aspx*').as('guardadoFinal');
 
         cy.get("input[id$='btnGuardar']").click({ force: true });
-        // 4. ESTO ES CLAVE: Esperar a que el servidor responda positivamente
+        // Esperar a que el servidor responda positivamente
         cy.wait('@guardadoFinal');
     });
 
@@ -112,14 +120,25 @@
         //Tipo de Muestra
         cy.get("input[id$='txtCodigoMuestra']").type("cm").type('{enter}');
       
+        //"LABORATORIO CENTRAL"
+        cy.fixture("datos").then(x => {
+            if (x.efector == "LABORATORIO CENTRAL") {
+                //Caracter SISA:
+                cy.get("select[id$='ddlCaracter']").select(x.caracterSisa);
+                //Nro. Hisopado
+                cy.get("input[id$='txtNumeroOrigen2']").type(x.numeroHisopado);
+                
+            }
+        });
 
+        //Tiempo de espera para el anthem
         cy.get('body').click(0, 0);
         cy.wait(1000);
         // 2. Interceptar el guardado para obligar a Cypress a esperar la respuesta
         cy.intercept('POST', '**/ProtocoloEdit2.aspx*').as('guardadoFinal');
 
         cy.get("input[id$='btnGuardar']").click({ force: true });
-        // 4. ESTO ES CLAVE: Esperar a que el servidor responda positivamente
+        // Esperar a que el servidor responda positivamente
         cy.wait('@guardadoFinal');
     });
 
