0ed608ceb99af745a7f52b0ce9d140af28b3074f
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2026-02-26 15:34:01 -0300

    fix: cartel de error para combinacion onelogin y externo

--- WebLab/Usuarios/UsuarioEdit.aspx.designer.cs
+++ WebLab/Usuarios/UsuarioEdit.aspx.designer.cs
@@ -258,6 +258,15 @@ namespace WebLab.Usuarios
         protected global::System.Web.UI.WebControls.CheckBox chkRequiereContrasenia;
 
         /// <summary>
+        /// customValidatorExterno control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator customValidatorExterno;
+
+        /// <summary>
         /// lnkRegresar control.
         /// </summary>
         /// <remarks>
