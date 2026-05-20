4065f15d8fcbefc3da4d6af6d4cd367450e51ea7
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2026-05-20 12:24:21 -0300

    fix: enconde de valores como + en la sanitacion

--- Business/Utility.cs
+++ Business/Utility.cs
@@ -1172,6 +1172,8 @@ namespace Business
             string html = server.UrlDecode(texto);
             // Sanitizar
             string limpio = Sanitizer.GetSafeHtmlFragment(html);
+            //Después del sanitizado, decodificá entidades HTML:
+            limpio = HttpUtility.HtmlDecode(limpio);
 
             return limpio;
         }
