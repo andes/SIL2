346c2bad0f8e076980d9b9c53a9f407d8b16cd16
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2026-08-20 12:06:53 -0300

    fix: Se agrega auditoria cuando se cambia "sin muestra" , se cambia el usuario de auditoria en eliminacion de analisis

--- WebLab/Resultados/AnalisisEdit.aspx.cs
+++ WebLab/Resultados/AnalisisEdit.aspx.cs
@@ -414,7 +414,10 @@ namespace WebLab.Resultados
                         foreach (DetalleProtocolo oDetalle in listadetalle)
                         {
                             if (trajomuestra == "true")
+                            { 
                                 oDetalle.TrajoMuestra = "No";
+                                oDetalle.GrabarAuditoriaDetalleProtocolo("Sin Muestra", oUser.IdUsuario); //20.08.2026 guardar auditoria de cambio de muestra
+                            }
                             else // (trajomuestra == "false"
                             {
                                 /* Bug sobre la edición de determinaciones con la marca “sin muestra”:
@@ -423,12 +426,12 @@ namespace WebLab.Resultados
                                  * Correcion: si en Validacion se cambia a "Con muestra",se regeneran los detalles faltantes de la practica
                                  */
 
-                               
+                                oDetalle.GrabarAuditoriaDetalleProtocolo("Con Muestra", oUser.IdUsuario); //20.08.2026 guardar auditoria de cambio de muestra
                                 bool antesSinMuestra = (oDetalle.TrajoMuestra == "No");
                                 oDetalle.TrajoMuestra = "Si";
-                                
-                                if (antesSinMuestra && oDetalle.IdItem == oDetalle.IdSubItem && 
-                                    (oRegistro.IdTipoServicio.IdTipoServicio == 1 || oRegistro.IdTipoServicio.IdTipoServicio ==3)) //Solo pasa en Labo y Microbiologia, en NO Pacientes se generan todos los items sin muesta
+
+                                if (antesSinMuestra && oDetalle.IdItem == oDetalle.IdSubItem &&
+                                    (oRegistro.IdTipoServicio.IdTipoServicio == 1 || oRegistro.IdTipoServicio.IdTipoServicio == 3)) //Solo pasa en Labo y Microbiologia, en NO Pacientes se generan todos los items sin muesta
                                 {
                                     /* Si ValidadoTotal (en ProtocoloEdit2) cargó idUsuarioValida por un caso de
                                     * "sin muestra"   se debe resetear idUsuarioValida para evitar inconsistencias 
@@ -436,8 +439,8 @@ namespace WebLab.Resultados
 
                                     //Caso 1:  el análisis aún no tiene resultados (simple o compuesto),
                                     if (!oDetalle.ConResultado)
-                                    { 
-                                        oDetalle.IdUsuarioValida = 0; 
+                                    {
+                                        oDetalle.IdUsuarioValida = 0;
                                         oDetalle.FechaValida = DateTime.Parse("01/01/1900");
                                     }
 
@@ -454,10 +457,10 @@ namespace WebLab.Resultados
                                      *  Considero idItem = idSubItem
                                      */
 
-                                    if ( oItem.IdCategoria == 1 )
+                                    if (oItem.IdCategoria == 1)
                                         GuardarDetallePractica(oDetalle, true); //true -->  se omite si es Derivacion para no generar una nueva Derivacion
                                 }
-                                
+
                             }
                             oDetalle.Save();
                         }
@@ -495,7 +498,7 @@ namespace WebLab.Resultados
                         if (noesta)
                         {
                             oDetalle.Delete();                            
-                            oDetalle.GrabarAuditoriaDetalleProtocolo("Elimina", int.Parse(Session["idUsuario"].ToString()));
+                            oDetalle.GrabarAuditoriaDetalleProtocolo("Elimina", oUser.IdUsuario); //20.08.2029 usar el oUser logueado (idUsuarioValida en validacion o idUsuario en carga)
                         }
                     }
                 }
