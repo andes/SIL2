f616c6d4b89d64630c727ee4da71bb1f332d4e9e
Vanesa Rimada <80859025+vrimada@users.noreply.github.com>
2026-09-07 11:56:43 -0300

    fix; fecha de modificacion de lote

--- WebLab/Derivaciones/InformeList3.aspx.cs
+++ WebLab/Derivaciones/InformeList3.aspx.cs
@@ -270,7 +270,7 @@ namespace WebLab.Derivaciones
                     m_strSQL += " FROM  vta_LAB_Derivaciones vta ";
                     m_strSQL += " left join LAB_DerivacionMotivoCancelacion mot on mot.idMotivo = vta.idMotivoCancelacion ";
                     m_strSQL += " WHERE    (" +
-                              " vta.fecha >='20260101' and " + //Fecha hardcodeada hasta la mejora de modificacion
+                              " vta.fecha >='" + DateTime.Today.AddDays(-14).ToString("yyyyMMdd") +"' and " + //Fecha de 14 dias hasta la mejora de modificacion
                               "    (estado = 0 and isnull(idlote,0) = 0 " +//Traer derivaciones pendientes por si se necesitan agregar 
                               "       and idEfectorDerivacion = " + Request["Destino"] + " and idEfector = " + oUser.IdEfector.IdEfector + ")   " +
                               "  or (estado = 4 and idLote= " + Request["idLote"] + ")" + //y ya cargadas en el lote por si se necesitan dejar nuevamente como pendiente
